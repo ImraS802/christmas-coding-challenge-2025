158a4e75d5aa4429a0d6ee7a108d2ccb3563492a
imra.s <i.skaliks.is@gmail.com>
2025-12-08 16:31:26 +0100

    Filter numbers greater than 10

---
+++ day-3.js
@@ -0,0 +1,10 @@
+/*
+Question:
+Use .filter() to create a new array that contains only the numbers greater than 10.
+const numbers = [1, 4, 7, 10, 13, 16];
+*/
+
+const numbers = [1, 4, 7, 10, 13, 16];
+let filtNumb = numbers.filter((numbers) => numbers > 10);
+
+console.log(filtNumb);
