b680661e3150c935b68dc277b030d7964cc0f122
imra.s <i.skaliks.is@gmail.com>
2025-12-26 16:46:14 +0100

    add .forEach()

--- day-10.js
+++ day-10.js
@@ -5,7 +5,7 @@ Implement a function to check and uncheck all checkboxes at once
 
 function checkUncheckAll() {
   const checkboxesSelected = document.querySelectorAll('.checkboxes');
-  console.log(checkboxesSelected);
+  //   console.log(checkboxesSelected);
 
   const allChecked = [...checkboxesSelected].every(
     (element) => element.checked
---
+++ day-11.js
@@ -0,0 +1,11 @@
+/*
+Question:
+You have a list of numbers.
+Print each number multiplied by 2.
+*/
+
+const numbersFor = [1, 2, 3, 4, 5];
+
+numbersFor.forEach((element) => {
+  console.log(element * 2);
+});
